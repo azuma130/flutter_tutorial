31c07fc7232f320d595410891afe4da5e5ce0c6b
youta.azuma.yw <youta.azuma.yw@nttqonoq.com>
2023-11-28 15:24:26 +0900

    チュートリアル

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter layout demo'),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(40),
+          color: Colors.yellowAccent,
+          child:
+              const Column(children: [Text('Hello World!'), Text('こんにちは世界！')]),
         ),
       ),
-    );
+    ));
   }
 }
